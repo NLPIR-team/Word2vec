569af58d96a5114aae4ed2b76273a254ec4a9bd9
Hongyan Pan <1107724781@qq.com>
2018-04-16 11:31:06 +0800

    add windows dll

--- src/com/pantek/test/Word2vec.java
+++ src/com/pantek/test/Word2vec.java
@@ -7,11 +7,11 @@ public class Word2vec {
 	public static void main(String[] args){
 		int i=CLibraryWord2vec.Instance.InitWord2vec("Data/","");
 		System.out.println(i);
-		CLibraryWord2vec.Instance.InitPara(200, "corpus.txt", "words-all.txt", 1, Float.parseFloat("0.025"), "vectors.txt", 8, Float.parseFloat("1e-4"), 1, 25, 1, 15, 1);
+		CLibraryWord2vec.Instance.InitPara(200, "dang.bin", "words-all.txt", 1, Float.parseFloat("0.025"), "vectors.txt", 8, Float.parseFloat("1e-4"), 1, 25, 1, 15, 1);
 		CLibraryWord2vec.Instance.Train();
 		CLibraryWord2vec.Instance.LoadModel("vectors.txt");
-		String result=CLibraryWord2vec.Instance.CalculateWord("中国",10);
-		String result1=CLibraryWord2vec.Instance.CalculateWord("秘鲁",10);
+		String result=CLibraryWord2vec.Instance.CalculateWord("红岩精神",10);
+		String result1=CLibraryWord2vec.Instance.CalculateWord("中国梦",10);
 		System.out.println(result);
 		System.out.println(result1);
 	}
